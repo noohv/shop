d0eb51d08b50e70283f57e4b71258a4fbddb2eb7
noohv <56311522+noohv@users.noreply.github.com>
2021-06-03 00:32:50 +0300

    Ordering
    Fixed some bugs with ordering and made new order history view

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -16,8 +16,8 @@ class OrderController extends Controller
 
     public function index() {
         $orders = Order::where("user_id", "=", Auth::id())->paginate(10);
-        $orderitems = OrderItem::get();
-        return view('user_orders',compact('orders','orderitems'));
+        // $orderitems = OrderItem::get();
+        return view('user_orders',compact('orders'));
     }
 
     public function store(Request $request)
@@ -35,15 +35,15 @@ class OrderController extends Controller
                 $orderitem = new OrderItem();
                 $orderitem->order_id = $order->id;
                 $orderitem->foods_id = $food;
-                $orderitem->quantity = 1;
+                $orderitem->quantity = $request->quantity;
                 $orderitem->price = $price;
 
                 $orderitem->save();
             }
+            session()->forget('foods'); // Removes foods from reserved
+            return redirect()->route('home');        
         }
 
-        session()->forget('foods'); // Removes books from reserved
-        return redirect()->route('home');        
     }
 
     /**
